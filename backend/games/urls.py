c6ff984380516a723c28a2794609af5cee121a9c
satyaakant <satyakantmishra50@gmail.com>
2026-03-11 13:03:10 +0530

    twice a day update for game

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', GameViewSet.as_view({'get': 'list'}), name='game-list'),
     path('<slug:slug>/leaderboard/', GameViewSet.as_view({'get': 'leaderboard'}), name='game-leaderboard'),
     path('<slug:slug>/my_transactions/', GameViewSet.as_view({'get': 'my_transactions'}), name='game-my-transactions'),
+    path('<slug:slug>/check_play_limit/', GameViewSet.as_view({'get': 'check_play_limit'}), name='game-check-play-limit'),
     path('<slug:slug>/award/', GameAwardView.as_view(), name='game_award_specific'),
 ]
